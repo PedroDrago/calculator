f8371c1287e2482a2a3ab111f8712c70660fd611
Drago <pedrodrago.profissional@gmail.com>
2022-06-22 12:49:23 -0300

    Add operate function

--- script.js
+++ script.js
@@ -14,16 +14,18 @@ function divide(number1, number2){
     return number1 / number2
 }
 
-// function operate(number1, number2, operator){
-//     if(operator==="+"){
+function operate(number1, number2, operator){
+    if(operator==="+"){
+        return sum(number1, number2)
+    }
+    else if(operator==="-"){
+        return subtract(number1, number2)
 
-//     }
-//     else if(operator==="-"){
+    }else if(operator==="*"){
+        return multiply(number1, number2)
         
-//     }else if(operator==="*"){
-        
-//     }else if(operator==="/"){
-        
-//     }
-// }
+    }else if(operator==="/"){
+        return divide(number1, number2)
+    }
+}
 
