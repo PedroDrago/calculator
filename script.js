2dab4bafc42eccd4eb84f474d6a4741e33dca9d3
Drago <pedrodrago.profissional@gmail.com>
2022-06-22 12:47:54 -0300

    Add operations functions in script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function sum(number1, number2){
+    return number1 + number2
+}
+
+function subtract(number1, number2){
+    return number1 - number2
+}
+
+function multiply(number1, number2){
+    return number1 * number2
+}
+
+function divide(number1, number2){
+    return number1 / number2
+}
+
+// function operate(number1, number2, operator){
+//     if(operator==="+"){
+
+//     }
+//     else if(operator==="-"){
+        
+//     }else if(operator==="*"){
+        
+//     }else if(operator==="/"){
+        
+//     }
+// }
+
